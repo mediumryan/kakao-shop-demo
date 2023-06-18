a45814d345bc70f9736871d14d62899087a9d8a9
Ryan <straightwind0726@gmail.com>
2023-06-18 22:31:04 +0900

    [Update] change from css to styled-components MainNav.js

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,38 +1,90 @@
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 // 로고 이미지 불러오기
-import mainLogo from './../images/main-logo.png';
+import mainLogo from "./../images/main-logo.png";
+// styled-components 불러오기
+import {
+  MainNavContainer,
+  NavTitle,
+  NavMenu,
+  NavOthers,
+  NavToggleBtn,
+} from "../styled/StyledNavbar";
 
 const MainNav = () => {
+  const navigate = useNavigate();
+  const [toggle, setToggle] = useState(true);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-  const navigate = useNavigate();  
-  const [toggle, setToggle] = useState(false);
-  
-  return(
-    <div id='navbar'>
-      <div className='nav-title' onClick={()=>{navigate('/')}}>
-        <img src={mainLogo}/>
-      </div>
-      <ul className={`nav-menu ${toggle == true && 'visible'}`}>
-        <li onClick={()=>{navigate('/')}}>홈</li>
-        <li onClick={()=>{navigate('/doll')}}>인형</li>
-        <li onClick={()=>{navigate('/umbrella')}}>우산</li>
-        <li onClick={()=>{navigate('/stationery')}}>문구류</li>
-        <li onClick={()=>{navigate('/bedding')}}>침구류</li>
-      </ul>
-      <ul className={`nav-others ${toggle == true && 'visible'}`}>
-        <li>🔔</li>
-        <li onClick={()=>{navigate('/cart')}}>🛒</li>
-        <li onClick={()=>{navigate('/signin')}}>👤</li>
-      </ul>
-      <button 
-        className='nav-toggle-btn'
-        onClick={()=>{
-          setToggle(!toggle);
+  return (
+    <MainNavContainer>
+      <NavTitle
+        onClick={() => {
+          navigate("/");
         }}
-        >☰</button>
-    </div>
-  )
-}
+      >
+        <h1>Ryan Shop</h1>
+        <img src={mainLogo} alt="카카오 캐릭터 로고 이미지입니다." />
+      </NavTitle>
+      <NavMenu show={toggle}>
+        <li
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          홈
+        </li>
+        <li
+          onClick={() => {
+            navigate("/doll");
+          }}
+        >
+          인형
+        </li>
+        <li
+          onClick={() => {
+            navigate("/umbrella");
+          }}
+        >
+          우산
+        </li>
+        <li
+          onClick={() => {
+            navigate("/stationery");
+          }}
+        >
+          문구류
+        </li>
+        <li
+          onClick={() => {
+            navigate("/bedding");
+          }}
+        >
+          침구류
+        </li>
+      </NavMenu>
+      <NavOthers show={toggle}>
+        <li>🔔</li>
+        <li
+          onClick={() => {
+            navigate("/cart");
+          }}
+        >
+          🛒
+        </li>
+        <li
+          onClick={() => {
+            navigate("/signin");
+          }}
+        >
+          👤
+        </li>
+      </NavOthers>
+      <NavToggleBtn onClick={handleToggle}>☰</NavToggleBtn>
+    </MainNavContainer>
+  );
+};
 
 export default MainNav;
---
+++ src/styled/StyledNavbar.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+export const MainNavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  padding: 0 3.5rem 0 7rem;
+  border-bottom: 2px solid var(--color-1);
+  position: relative;
+  @media screen and (max-width: 413px) {
+    flex-direction: column;
+    padding: 0;
+  }
+`;
+
+export const NavTitle = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  h1 {
+    color: #645cbb;
+    font-size: 28px;
+    @media screen and (max-width: 413px) {
+      display: none;
+    }
+  }
+  img {
+    height: 120px;
+  }
+`;
+
+const NavLists = styled.li``;
+
+export const NavMenu = styled.ul`
+  display: ${(props) => (props.show ? "flex" : "none")};
+  margin: 0 2rem;
+  li {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    font-weight: var(--font-weight-bold);
+    margin-right: 0.5rem;
+    border-bottom: 1px solid transparent;
+    transition: var(--animation-duration) all;
+    &:hover {
+      border-bottom: 1px solid var(--color-1);
+    }
+    @media screen and (max-width: 413px) {
+      margin: 0;
+      width: 100%;
+      padding: 0;
+      padding-bottom: 12px;
+      margin-bottom: 12px;
+      text-align: center;
+    }
+  }
+  @media screen and (max-width: 413px) {
+    flex-direction: column;
+    margin: 0;
+    width: 100%;
+  }
+`;
+
+export const NavOthers = styled.ul`
+  display: ${(props) => (props.show ? "flex" : "none")};
+  margin-left: auto;
+  li {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    font-weight: var(--font-weight-bold);
+    margin-right: 0.5rem;
+    border-bottom: 1px solid transparent;
+    transition: var(--animation-duration) all;
+    &:hover {
+      border-bottom: 1px solid var(--color-1);
+    }
+  }
+  @media screen and (max-width: 413px) {
+    margin: 12px auto;
+  }
+`;
+
+export const NavToggleBtn = styled.button`
+  display: none;
+  position: absolute;
+  right: 24px;
+  top: 42px;
+  border: none;
+  background: none;
+  outline: none;
+  font-size: 24px;
+  @media screen and (max-width: 413px) {
+    display: block;
+  }
+`;
